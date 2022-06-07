ec2a9b3a1e13a7c198414c8c1d2c3275a066a61b
lim-jiwoo <il04131@naver.com>
2022-06-07 18:16:28 +0900

    feat(server): make getPosts, createPosts controllers

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,22 @@
+import Artwork from '../models/Artwork.js';
 
-export const getPosts = (req, res) => {
-    res.send('THIS WORKS')
+export const getPosts = async (req, res) => {
+    try {
+        const artworks = await Artwork.find();
+        res.status(200).json(artworks);
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
 }
+
+export const createPosts = async (req, res) => {
+    const body = req.body;
+    const newArtwork = new Artwork(body);
+    
+    try {
+        await newArtwork.save();
+        res.statu(201).json(newArtwork);
+    } catch (error) {
+        res.status(409).json({message: error.message});
+    }
+} 
