d0947765a9878f5abb7e45c780ba47204fc84fa6
lim-jiwoo <il04131@naver.com>
2022-06-07 16:10:11 +0900

    docs(server): make basic folder structure for server-side routing

--- server/index.js
+++ server/index.js
@@ -2,11 +2,15 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
 
 app.use(express.json({limit:'30mb', extended:true}));
 app.use(express.urlencoded({limit:'30mb', extended:true}));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://master:mojiwoo5516@nodetuts.p7k9x.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
