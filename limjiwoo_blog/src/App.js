fe56394617afc7ca8c42658defd58d30f2d34dc3
lim-jiwoo <il04131@naver.com>
2022-06-07 19:32:24 +0900

    feat(client): implement getPosts action

--- limjiwoo_blog/src/App.js
+++ limjiwoo_blog/src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 import './App.css';
 import { About, Artwork, Artworks, Cover, Navbar } from './containers';
+import { getPosts } from './actions/posts';
 
 const App = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch]);
+
     return (
         <div className="App">
             <Navbar />
