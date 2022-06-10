313935a3945eed44c6e6aac7966e109d0d303b71
lim-jiwoo <il04131@naver.com>
2022-06-10 23:02:52 +0900

    feat(client): make basic structure of Artwork (including title, image and tags)

--- limjiwoo_blog/src/containers/Artwork/Artwork.jsx
+++ limjiwoo_blog/src/containers/Artwork/Artwork.jsx
@@ -3,8 +3,22 @@ import React from 'react';
 import './artwork.css';
 
 const Artwork = () => {
+
+  const tmp = {title:'Work', tags:['1','2'], 
+  // image: 'https://images.squarespace-cdn.com/content/v1/57f0b8c859cc68fe6a908fb7/1562232357650-4K0I89UWU3SYRTKEYSD1/cypresses-by-vincent-van-gogh-famous-art-handmade-oil-painting-on-canvas.jpg?format=2500w'
+  image: 'https://wallpaperaccess.com/full/2656.jpg'
+};
+
   return (
-    <div>Artwork</div>
+    <div className="blog__artwork">
+      <div className="blog__artwork-img">
+        <img src={tmp.image} alt="artwork" />
+      </div>
+      <div className="blog__artwork-description">
+        <h1 className="blog__artwork-title">{tmp.title}</h1>
+        <p className="blog__artwork-tags">{tmp.tags.map(tag => '#'+tag+' ')}</p>
+      </div>
+    </div>
   )
 }
 
